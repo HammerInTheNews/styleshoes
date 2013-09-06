0b10519dc32621fcea999a2a198ff02ff7370560
aljudson <alanjudson@gmail.com>
2013-09-06 13:25:12 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Styleshoes::Application.config.secret_token = 'ed123c679a46a57dfd10b4713fc08a1be04b8ac298c5a03e9268258cbba0954c4ebd1b00328182dff88bb057486553ff9f60188e2914ea58eed19d78f37c2c01'
